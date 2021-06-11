ceafde6f59a176f0bfeece7d5e6002a0234eb579
jorgefortunatof <jorgefortunatof@gmail.com>
2021-06-10 23:06:56 -0300

    Added clients table creation migration

--- src/database/migrations/1623376019097-CreateClients.ts
+++ src/database/migrations/1623376019097-CreateClients.ts
@@ -1,7 +1,43 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateClients1623376019097 implements MigrationInterface {
-	public async up(queryRunner: QueryRunner): Promise<void> {}
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: "clients",
+				columns: [
+					{
+						name: "id",
+						type: "int",
+						isPrimary: true,
+					},
+					{
+						name: "cpf",
+						type: "varchar(11)",
+						isNullable: false,
+						isUnique: true,
+					},
+					{
+						name: "nome",
+						type: "varchar(60)",
+						isNullable: false,
+					},
+					{
+						name: "created_at",
+						type: "timestamp",
+						default: "now()",
+					},
+					{
+						name: "updated_at",
+						type: "timestamp",
+						default: "now()",
+					},
+				],
+			})
+		);
+	}
 
-	public async down(queryRunner: QueryRunner): Promise<void> {}
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable("clients");
+	}
 }
